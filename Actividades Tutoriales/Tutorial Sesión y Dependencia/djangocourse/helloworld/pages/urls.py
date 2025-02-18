21cc038c179312a2d0bd24c04bd288a6d2d5b68b
JJCanon <jjcanond@eafit.edu.co>
2025-02-18 00:32:01 -0500

    Tutorial 3a - Sesion y dependencia

---
+++ Actividades Tutoriales/Tutorial Sesión y Dependencia/djangocourse/helloworld/pages/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView,CartView,CartRemoveAllView
+ 
+ 
+urlpatterns = [ 
+    path("", HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create', ProductCreateView.as_view(), name='form'), 
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+]
